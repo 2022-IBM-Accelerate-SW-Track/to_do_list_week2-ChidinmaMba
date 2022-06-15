02aea3a9a44e8a2b23cdd37da05a3f6c2efc33f8
ChidinmaMba <mbachidinma4@gmail.com>
2022-06-15 19:19:52 -0400

    assignment two completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import Todos from "../component/todos";
-import AddTodo from "../component/AddTodo";
-import "../pages/Home.css";
-
+import React, { Component } from 'react';
+import AddTodo from '../component/AddTodo';
+import Todos from "../component/todos.js";
+import "./Home.css";
+  
 class Home extends Component {
-  // Create a default state of this component with an empty list of todos.
+  // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
-      todos: [],
+      // create your empty list here call it todos.
+      todos: []
     };
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
@@ -19,24 +20,31 @@ class Home extends Component {
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
-    todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
+    if (this.state.todos.includes(todo)){
+      return;
+    } else{
+      todo.id = Math.random();
+      // An array that contains the current array and the new todo item
+      let new_list = [...this.state.todos, todo];
+      // Updates the local state with the new array.
+      this.setState({
+        todos: new_list,
+      });
+    }
+  };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
     this.setState({
-      todos: new_list,
+      todos: todos,
     });
   };
   render() {
     return (
       <div className="Home">
-        <h1>Todo's </h1>
-        {/* When passing the AddTodo component, addTodo is a prop that is used in the 
-        AddTodo.js file when handling the submit */}
-        <AddTodo addTodo={this.addTodo} />
-        {/* When returning the Todos component, todos is a prop passed to the todos.js file
-         to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <AddTodo addTodo={ this.addTodo } />
+        <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } />
       </div>
     );
   }
